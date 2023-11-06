71094e6138b26e3c8ebe804e4dd87afcd9258b9f
gdavis <gregory.davis@autozone.com>
2023-11-05 21:23:14 -0500

    fix(request): removed unnecessary calls to ai chat

--- utils/localStorage.tsx
+++ utils/localStorage.tsx
@@ -2,7 +2,7 @@ import root from "window-or-global";
 import { PersistentStorage } from "@/utils/PersistentStorage";
 import type { Storage } from "@/utils/PersistentStorage";
 
-type LocalStorageKey = "childName" | "childGender" | "childAge" | "childStory";
+type LocalStorageKey = "childName" | "childGender" | "childAge" | "childStory" | "aiPageData";
 
 export const localStorage: Readonly<Storage<LocalStorageKey>> =
   new PersistentStorage<LocalStorageKey>(root.localStorage);
